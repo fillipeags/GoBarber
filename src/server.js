1b4b79ac0b89e7f9c675c3770ba80d06c453c051
Fillipe <fillipe484@gmail.com>
2020-08-21 20:26:39 -0300

    setting up eslint | prettier | editorConfig

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
-import app from './app';
+import app from "./app";
 
-app.listen(3333, console.log('🔥 Server started running at http://localhost:3333'));
+// eslint-disable-next-line no-console
+app.listen(
+  3333,
+  // eslint-disable-next-line no-console
+  console.log("🔥 Server started running at http://localhost:3333")
+);
