0f2a590161de9d9ba29bcabc749e4a8cccbca5be
Fillipe <fillipe484@gmail.com>
2020-08-24 16:37:05 -0300

    middleware user authentication

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -16,6 +16,12 @@ class UserController {
       id, name, email, provider,
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
