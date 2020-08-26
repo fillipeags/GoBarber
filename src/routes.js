57f771d208c52e42d8986072e780baf5f5e2967f
Fillipe <fillipe484@gmail.com>
2020-08-25 21:21:35 -0300

    user avatar
    dealing with relationship between avatar up and user

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,9 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
-import authMiddleware from './app/middlewares/auth';
+import authMiddleware from './app/controllers/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -17,6 +18,6 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => res.json({ ok: true }));
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
